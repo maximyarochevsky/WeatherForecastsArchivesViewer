ada9ffa6c0539dcd66080cb23377a22e213a9f42
maximyarochevsky <maximyarochevskygithub@gmail.com>
2024-03-27 13:41:33 +0300

    Основные проекты, сущности, группы сервисов

---
+++ Core/WeatherForecastsArchivesViewer.Domain/Enums/WideDirection.cs
@@ -0,0 +1,25 @@
+﻿namespace WeatherForecastsArchiveViewer.Domain.Enums;
+
+/// <summary>
+/// Направления ветра.
+/// </summary>
+public enum WindDirection
+{
+    North,
+    West,
+    South,
+    East,
+    Northwest,
+    Northeast,
+    Southwest,
+    Southeast,
+    NorthNorthwest,
+    NorthNortheast,
+    SouthSouthwest,
+    SouthSoutheast,
+    WestSouthwest,
+    EastSoutheast,
+    WestNorthwest,
+    EastNortheast,
+    Calm
+}
---
+++ Infrastructure/WeatherForecastsArchivesViewer.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WeatherForecastsArchiveViewer.Infrastructure;
+
+/// <summary>
+/// Статический класс расширений для <see cref="IServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+   
+}
